40de605c379cd4a6bba7d1c6ffa2bd28e4363cbb
Charles Kulkauski <charles.a.kulkauski@accenture.com>
2023-05-30 15:12:52 -0300

    Finalizando pesquisa de Clientes por Parâmetros

---
+++ Spring Web - Desenvolvimento da API RESTFUL/vendasJPA/src/main/java/io/github/charleskulkauski/domain/entity/Pedido.java
@@ -0,0 +1,80 @@
+package io.github.charleskulkauski.domain.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    //Referenciando os relacionamentos, muitos para um
+    //Juntando na coluna cliente_id
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @Column(name = "data_pedido")
+    private LocalDate dataPedido;
+
+    //nome da coluna, total de digitos, quatas casas decimais
+    // exemplo: 1000.00
+    @Column(name = "total", precision = 20, scale = 2)
+    private BigDecimal total;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(LocalDate dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "PEDIDO \n" +
+                "[ID]: ZN\n" + id +
+                "[DATA DO PEDIDO]: \n" + dataPedido +
+                "[TOTAL]: " + total ;
+    }
+}
